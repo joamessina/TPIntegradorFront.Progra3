4ba5171db70494d22c7ff1ee4934b402245241c7
KevinPlucci <kmplucci9911@gmail.com>
2025-07-07 18:39:14 -0300

    Actualizacion un poco

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 document
-  .getElementById('login-form')
-  .addEventListener('submit', async function (e) {
+  .getElementById("login-form")
+  .addEventListener("submit", async function (e) {
     e.preventDefault();
 
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
 
     try {
       const response = await fetch(
-        'http://localhost:3000/api/auth/login-client',
+        "http://localhost:3000/api/auth/login-client",
         {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
         }
       );
@@ -19,14 +19,26 @@ document
       const data = await response.json();
 
       if (response.ok) {
-        localStorage.setItem('token', data.token);
-        window.location.href = '../pages/index.html';
+        // Guardar el token
+        localStorage.setItem("token", data.token);
+
+        // Guardar LOG de login (hora y usuario)
+        const logs = JSON.parse(
+          localStorage.getItem("logs_login_pokemon") || "[]"
+        );
+        logs.push({
+          usuario: email,
+          fecha: new Date().toLocaleString("es-AR"),
+        });
+        localStorage.setItem("logs_login_pokemon", JSON.stringify(logs));
+
+        window.location.href = "../pages/index.html";
       } else {
-        document.getElementById('login-error').textContent =
-          data.error || 'Error de login';
+        document.getElementById("login-error").textContent =
+          data.error || "Error de login";
       }
     } catch (error) {
-      document.getElementById('login-error').textContent =
-        'No se pudo conectar con el servidor';
+      document.getElementById("login-error").textContent =
+        "No se pudo conectar con el servidor";
     }
   });
